ec10f5a6437ac66dc1203a8da1212fb3ba92a4d2
Athitaya Saartaiam <athitaya5678@gmail.com>
2024-03-01 01:13:54 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $product = Product::all();
+    return view('welcome',compact('product'));
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/type', function () {
+    $product = Product::all();
+    return view('type',compact('product'));
+});
+ 
+Route::get('/performance', function () {
+    return view('performance');
+});
+
+Route::get('/dashboard', function () {
+    $pro = Product::all();
+    $user = User::all();
+    return view('dashboard',compact('pro','user'));
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+//เมนู Product
+Route::get('admin/product/index',[ProductController::class, 'index'])->name('p.index');
+Route::get('admin/product/create',[ProductController::class, 'create']);
+Route::post('admin/product/insert',[ProductController::class, 'insert']);
+Route::get('admin/product/delete/{id}',[ProductController::class, 'delete']);
+Route::get('admin/product/edit/{id}',[ProductController::class, 'edit']);
+Route::post('admin/product/update/{id}',[ProductController::class, 'update']);
